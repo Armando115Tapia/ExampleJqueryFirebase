5adda63354ff4917753f918ebcde3a6836e419e9
Armando Tapia <jusnfds123@hotmail.com>
2019-01-06 16:03:14 -0500

    basic functionallity of a Jquery

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 //Use of JQUERY
+//login
+var provider = new firebase.auth.GoogleAuthProvider();
+
+//Simple use of jquery 
+// -> sintax of jquery $('')
+//Obsrver click button
+$('#login').click( function () {
+        firebase.auth()
+        .signInWithPopup(provider)
+        .then(function(result) {
+            console.log(result.user);
+        }); 
+
+}); 
 
 
-//login 
+
+
+
+//
